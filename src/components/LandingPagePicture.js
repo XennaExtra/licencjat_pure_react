a4affa545e634658fbe589107955296a1ecb3cc5
XeNeeX <kamiljarmolowicz9@gmail.com>
2023-10-13 19:31:31 +0200

    Conversion to pure React

---
+++ src/components/LandingPagePicture.js
@@ -0,0 +1,10 @@
+import './component_css/landing_picture.scss'
+import landing_pic from './component_elements/landing_pic.jpg'
+export function LandingPagePicture(){
+    return(
+        <main className={'landingPicBox'}>
+            <img src={landing_pic} alt={"Landing Page Image"}/>
+        </main>
+    )
+}
+
