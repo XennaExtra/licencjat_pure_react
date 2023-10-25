125634291ebbf7ed1419fc8d27bc735093c26bf1
Kamil Jarmołowicz <100926787+XennaExtra@users.noreply.github.com>
2023-10-25 22:14:32 +0200

    Add files via upload

--- src/frontend/components/ContactForm.js
+++ src/frontend/components/ContactForm.js
@@ -1,20 +1,39 @@
 import React from 'react'
+import { Component } from 'react'
 import './component_css/contact_form.scss'
+import axios from 'axios'
 
-export function ContactForm() {
+export class ContactForm extends Component {
+  constructor(){
+    super();
+    this.state = {
+      data: [],
+    }
+  }
+
+  getData = () => {
+    axios.post("../../backend/db_connect.php")
+      .then(response => {
+        this.setState({dane: response.data});
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  }
+  render() {
   return (
     <>
         <h1>Skontaktuj się z nami!</h1>
-        <form className='contactForm' action=''>
-            <label for='full_name'><h2>Imię i nazwisko*</h2></label>
+        <form className='contactForm' action={this.getData()}>
+            <label htmlFor='full_name'><h2>Imię i nazwisko*</h2></label>
             <input type='text' name='full_name' placeholder='Jan Kowalski' required></input>
-            <label for='patient_id'><h2>Numer pacjenta`</h2></label>
+            <label htmlFor='patient_id'><h2>Numer pacjenta`</h2></label>
             <input type='number' name='patient_id' placeholder=""></input>
-            <label for='patient_email'><h2>Adres e-mail*</h2></label>
+            <label htmlFor='patient_email'><h2>Adres e-mail*</h2></label>
             <input type='email' name='patient_email' placeholder="jan.kowalski@poczta.pl" required></input>
-            <label for='patient-message'><h2>Twoja wiadomość*</h2></label>
+            <label htmlFor='patient-message'><h2>Twoja wiadomość*</h2></label>
             <textarea name='patient_message' placeholder="Twoja wiadomość" required/>
-            <label for='msgSubmit'></label>
+            <label htmlFor='msgSubmit'></label>
             <input name='msgSubmit'type='submit' value='Wyślij wiadomość' className='submitButton'></input>
         </form>
         <h5>* - pole wymagane</h5>
@@ -22,4 +41,4 @@ export function ContactForm() {
         <h6>Wysyłając wiadomość, zgadzasz się na przetwarzanie swoich danych osobowych przez Praktykę Lekarską w celu odpowiedzi na twoją <br/> wiadomość poprzez email, bądź konto pacjenta. Administratorem danych osobowych jest Praktyka Lekarska. <br/> Dane osobowe nie są wykorzystywane w celach innych niż kontakt z pacjentem.</h6>
     </>
   )
-}
+}}
